668a4c6e2feeb7338b69bffa888f3a7969d53e00
leomet07 <leomet07@gmail.com>
2022-01-12 13:50:42 -0500

    Added analytics to see who scans QR code

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,33 @@
 	import { Router, Link, Route } from "svelte-routing";
 	import Home from "./routes/Home.svelte";
 	import About from "./routes/About.svelte";
+	import { onMount } from "svelte";
 
 	export let url = "";
+
+	import { initializeApp } from "firebase/app";
+	import { getAnalytics } from "firebase/analytics";
+	onMount(async () => {
+		// Import the functions you need from the SDKs you need
+		// TODO: Add SDKs for Firebase products that you want to use
+		// https://firebase.google.com/docs/web/setup#available-libraries
+
+		// Your web app's Firebase configuration
+		// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+		const firebaseConfig = {
+			apiKey: "AIzaSyDIi78kidzG_d2EsK9MgOftDTLsUQMkeDs",
+			authDomain: "ikeabridge-analytics.firebaseapp.com",
+			projectId: "ikeabridge-analytics",
+			storageBucket: "ikeabridge-analytics.appspot.com",
+			messagingSenderId: "617936178141",
+			appId: "1:617936178141:web:8ec595045f9e3fa5f10434",
+			measurementId: "G-J5SHLT16MK",
+		};
+
+		// Initialize Firebase
+		const app = initializeApp(firebaseConfig);
+		const analytics = getAnalytics(app);
+	});
 </script>
 
 <Router {url}>
